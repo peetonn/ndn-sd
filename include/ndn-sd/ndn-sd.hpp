b43c3bbcc04942f3d1aa14c1cb568c2991684657
peetonn <gpeetonn@gmail.com>
2021-08-18 16:52:15 +0200

    [pg] first commit

---
+++ include/ndn-sd/ndn-sd.hpp
@@ -0,0 +1,7 @@
+
+#include <string>
+#include <ndn-ind/interest.hpp>
+
+namespace ndnsd {
+    std::string getVersionString();
+}
---
+++ src/ndn-sd/ndn-sd.cpp
@@ -0,0 +1,8 @@
+
+#include "ndn-sd.hpp"
+#include <ndn-ind/interest.hpp>
+
+std::string getVersionString()
+{
+    return "x.x.x";
+}
